9b8806c579024e78590b98796ff8b910c0cdc23a
avinashlatha13 <31725751+avinashlatha13@users.noreply.github.com>
2017-09-28 09:14:56 +0000

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 a=77
+#addition  operation
 print(a)
+#addition a with 5
 print(a+5)
+#subraction a with 22
 print(a-22)
+#multiplying a with 3
 print(a*3)
+#diving a with 11
 print(a/11)
+#percentage operation
 print(a%2)
